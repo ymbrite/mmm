24ce3e816b500e87f082283d41abaddc412257b1
ymbrite <132761445+ymbrite@users.noreply.github.com>
2026-03-26 22:07:45 -0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app *.github.io *.google.com codepen.io
+  frame-src giscus.app *.github.io *.google.com codepen.io *.youtube.com
 `
 
 const securityHeaders = [
